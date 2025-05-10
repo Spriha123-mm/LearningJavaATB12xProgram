6d292124ae9251a8045722780c88390ce1166441
SPRIHA SRIVASTAVA <srivastavaspriha769@gmail.com>
2025-05-10 11:31:02 +0530

    Merge branch 'main' of https://github.com/Spriha123-mm/LearningJavaATB12xProgram

---
+++ src/ex_01_Java_Basics/Lab_02.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basics;
+
+public class Lab_02 {
+
+public static void main(String[] args) {
+    System.out.println(2+2);
+    System.out.println(2*2);
+    System.out.println(2-2);
+    System.out.println(2+2+7);
+    System.out.print(4*2);
+
+
+}
+}
