61f134230a79963202a74fcd53412c308ac06e2c
SPRIHA SRIVASTAVA <srivastavaspriha769@gmail.com>
2025-05-15 15:26:56 +0530

    Merge branch 'main' of https://github.com/Spriha123-mm/LearningJavaATB12xProgram

---
+++ src/ex_03_Literals/Lab_23_Literal_basic1.java
@@ -0,0 +1,14 @@
+package ex_03_Literals;
+
+public class Lab_23_Literal_basic1 {
+    public static void main(String[] args) {
+        int age = 65;
+        // Literal - Integral
+
+        final float pi = 3.14f;
+        //  Literal - float
+
+        System.out.println(pi); // print the literal
+        System.out.println(age); // print the literal
+    }
+}
---
+++ src/ex_03_Literals/Lab_25_Boolean_literal.java
@@ -0,0 +1,11 @@
+package ex_03_Literals;
+
+public class Lab_25_Boolean_literal {
+
+    public static void main(String[] args) {
+
+        boolean Is= true;
+        boolean  talk= false;
+
+    }
+}
