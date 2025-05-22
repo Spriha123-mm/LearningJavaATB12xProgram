ffccf0fae0048b9f4346ce0848e3626da2cce2b8
SPRIHA SRIVASTAVA <srivastavaspriha769@gmail.com>
2025-05-22 16:47:54 +0530

    Merge branch 'main' of https://github.com/Spriha123-mm/LearningJavaATB12xProgram

---
+++ src/ex_08_If_Else_Condition_Statements/Lab_75_Practice_multipleinput_scaneerclass.java
@@ -0,0 +1,27 @@
+package ex_08_If_Else_Condition_Statements;
+
+import java.util.Scanner;
+
+public class Lab_75_Practice_multipleinput_scaneerclass {
+
+    public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(System.in);
+
+
+        System.out.println("Enter your name");
+        String name= scanner.nextLine();
+
+        System.out.println("Enter your age");
+        int age= scanner.nextInt();
+
+        System.out.println("Enter your salary");
+        double salary= scanner.nextDouble();
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(salary);
+
+
+    }
+}
