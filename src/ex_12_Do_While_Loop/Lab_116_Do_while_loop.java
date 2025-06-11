7072f1014b0a62f408cf156e9ca3c58cb882ecc9
SPRIHA SRIVASTAVA <srivastavaspriha769@gmail.com>
2025-06-11 12:53:53 +0530

    Merge branch 'main' of https://github.com/Spriha123-mm/LearningJavaATB12xProgram

---
+++ src/ex_12_Do_While_Loop/Lab_116_Do_while_loop.java
@@ -0,0 +1,14 @@
+package ex_12_Do_While_Loop;
+
+public class Lab_116_Do_while_loop {
+    public static void main(String[] args) {
+
+        int i=0;
+        do{
+            System.out.println("I am going to execute once");
+            i--;
+        }
+        while(i>-10);
+
+    }
+}
