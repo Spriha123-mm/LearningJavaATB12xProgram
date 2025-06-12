e54b10289ea62b42e1c77924e4d1406f1b59fa3b
SPRIHA SRIVASTAVA <srivastavaspriha769@gmail.com>
2025-06-12 15:49:00 +0530

    Merge branch 'main' of https://github.com/Spriha123-mm/LearningJavaATB12xProgram

---
+++ src/Tasks/Reverse_string_Inbuild_function.java
@@ -0,0 +1,12 @@
+package Tasks;
+
+public class Reverse_string_Inbuild_function {
+    public static void main(String[] args) {
+
+       StringBuffer reverse= new StringBuffer("I LOVE JAVA");
+       reverse=reverse.reverse();
+       System.out.println(reverse);
+
+
+    }
+}
