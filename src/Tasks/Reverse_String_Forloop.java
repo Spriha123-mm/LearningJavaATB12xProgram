aaab9239cf0d5320c3eb6ece45c7c4e95854c62f
SPRIHA SRIVASTAVA <srivastavaspriha769@gmail.com>
2025-06-12 15:58:02 +0530

    Merge branch 'main' of https://github.com/Spriha123-mm/LearningJavaATB12xProgram

---
+++ src/Tasks/Reverse_String_Forloop.java
@@ -0,0 +1,16 @@
+package Tasks;
+
+public class Reverse_String_Forloop {
+    public static void main(String[] args) {
+        String r="spriha Srivastava";
+
+        for (int i= r.length()-1;i>0;i--){
+
+            System.out.print(r.charAt(i));
+
+
+        }
+    }
+
+
+}
