7c75327f377c427ee9f76adbef803f07ed035b1e
SPRIHA SRIVASTAVA <srivastavaspriha769@gmail.com>
2025-06-12 16:16:54 +0530

    Merge branch 'main' of https://github.com/Spriha123-mm/LearningJavaATB12xProgram

---
+++ src/Tasks/Number_of_words_in_string.java
@@ -0,0 +1,16 @@
+package Tasks;
+
+public class Number_of_words_in_string {
+
+    public static void main(String[] args) {
+
+        String r="Spriha is good";
+
+        String[]words=r.split("\\s+");
+
+        System.out.println("the count of the number is\t" +words.length);
+
+
+
+    }
+}
