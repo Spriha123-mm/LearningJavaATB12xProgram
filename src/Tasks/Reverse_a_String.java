24e3de6e2987ead23a7749930de64a34e9966ed2
SPRIHA SRIVASTAVA <srivastavaspriha769@gmail.com>
2025-06-13 14:19:53 +0530

    Merge branch 'main' of https://github.com/Spriha123-mm/LearningJavaATB12xProgram

---
+++ src/Tasks/Reverse_a_String.java
@@ -0,0 +1,14 @@
+package Tasks;
+
+public class Reverse_a_String {
+
+    public static void main(String[] args) {
+
+        int[] a={1,8,9,0,0,6,2};
+
+        for(int i=a.length-1;i>=0;i--){
+
+            System.out.print(a[i]);
+        }
+    }
+}
