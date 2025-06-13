c1669433a39e070f5fdf87e5aff736415f0ce0cb
SPRIHA SRIVASTAVA <srivastavaspriha769@gmail.com>
2025-06-13 15:06:10 +0530

    Merge branch 'main' of https://github.com/Spriha123-mm/LearningJavaATB12xProgram

---
+++ src/Tasks/Addition_of_Arrays.java
@@ -0,0 +1,16 @@
+package Tasks;
+
+public class Addition_of_Arrays {
+    public static void main(String[] args) {
+        int [] array={12,12,12};
+
+        int sum=0;
+
+        for(int i= 0 ;i< array.length;i++){
+
+            sum=sum+array[i];
+        }
+        System.out.print(sum);
+    }
+    }
+
